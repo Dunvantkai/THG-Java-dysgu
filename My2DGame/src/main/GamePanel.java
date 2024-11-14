8b0134a7b42def7be17e2c3acf69f5bc996afe07
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2024-11-14 20:50:38 +1300

    Update GamePanel.java

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -47,15 +47,34 @@ public class GamePanel extends JPanel implements Runnable
 	@Override
 	public void run() 
 	{
-		double drawInterval = 1000000000/FPS;
+		double drawInterval = 1000000000/FPS; //
+		double nextDrawTime = System.nanoTime() + drawInterval;
+		
 		while(gameThread != null)
 		{
-//			long currentTime = System.nanoTime();
-//			System.out.println("CurrentTime "+currentTime);
-			
-			
+
 			update();
+			
 			repaint();
+			
+			
+			try 
+			{
+				double remainingTime = nextDrawTime - System.nanoTime();
+				//remainingTime = remainingTime /1000000;
+				if(remainingTime <0)
+				{
+					remainingTime = 0;
+				}
+				
+				Thread.sleep((long) remainingTime/1000000);
+				
+				nextDrawTime += drawInterval;
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	public void update() 
